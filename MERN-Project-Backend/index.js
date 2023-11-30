f43666109a727e605ecc7f1506544d66ba80e399
Aryan Singh <137106002+aryanmarshian@users.noreply.github.com>
2023-11-30 18:07:13 +0530

    Add files via upload

---
+++ MERN-Project-Backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+// const bodyParser = require("body-parser")
+const app = express()
+const Routes = require("./routes/route.js")
+
+const PORT = process.env.PORT || 2901
+
+const DB ='mongodb+srv://miasa:miasa%40123@cluster0.hxli8yn.mongodb.net/MIASA?retryWrites=true&w=majority';
+
+dotenv.config();
+
+// app.use(bodyParser.json({ limit: '10mb', extended: true }))
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+
+app.use(express.json({ limit: '10mb' }))
+app.use(cors())
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+app.use('/', Routes);
+
+app.listen(PORT, () => {
+    console.log(`Server started at port no. ${PORT}`)
+})
